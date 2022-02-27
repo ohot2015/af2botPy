1d12422f4616b199cbc9f7f830ffb37ba98a1e0a
banel1ng <ohot2015@gmail.com>
2022-02-27 21:26:56 +0300

    add stop and exit bot

--- main.py
+++ main.py
@@ -11,11 +11,35 @@ def check_exit():
         return True
 
 
+def pixel_search(rect, rgb_search, allowance=1):
+    screenshot = pyautogui.screenshot()
+    if allowance == 0:
+        allowance = 1
+    for i in range(rect[0], rect[2]):
+        for j in range(rect[1], rect[3]):
+            rgb_screen = screenshot.getpixel((i, j))
+            for allowance_r in range(0, allowance):
+                for allowance_g in range(0, allowance):
+                    for allowance_b in range(0, allowance):
+                        print(i, j, rgb_screen)
+                        r = rgb_search[0] + allowance_r
+                        g = rgb_search[1] + allowance_g
+                        b = rgb_search[2] + allowance_b
+                        if rgb_screen[0] == r and rgb_screen[1] == g and rgb_screen[2] == b:
+                            print(i, j)
+                            return True
+    return False
+
+
 def main():
     print('Нажмите Ctrl-C для выхода.')
     print("width =", GetSystemMetrics(0))
     print("height =", GetSystemMetrics(1))
     switch = 1
+
+    rect = (0, 0, 130, 90)
+    rgb_serach = (255, 0, 0)
+
     try:
         while check_exit():
             if keyboard.is_pressed('f10'):
@@ -26,8 +50,12 @@ def main():
                     switch = abs(abs(0 - switch) - 1)
                     time.sleep(0.5)
                 check_exit()
+                if pixel_search(rect, rgb_serach, 10):
+                    print('gooooooood')
+                return False
                 # Получение и вывод координат курсора, а также RGB-цвета пикселя под ним
                 x, y = pyautogui.position()
+
                 positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
                 pixelColor = pyautogui.screenshot().getpixel((x, y))
                 positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3)
@@ -42,4 +70,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
